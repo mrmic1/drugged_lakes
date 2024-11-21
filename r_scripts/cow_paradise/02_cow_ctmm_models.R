e855be801b7d1ee3f9713896c0207c459d07d63c
Marcus Michelangeli <marcus.michelangeli@gmail.com>
2024-11-21 23:59:12 +0000

    Add cow paradise scripts

--- r_scripts/cow_paradise/02_cow_ctmm_models.R
+++ r_scripts/cow_paradise/02_cow_ctmm_models.R
@@ -1,64 +1,63 @@
 #--------------------------------------#
-# Run CTMM models for LAke Cow Paradise #
+# Run CTMM models for Lake Cow Paradise #
 #--------------------------------------#
 
-# >1. Setup ####
+# > 1. Setup ####
 
 ###  LIBRARIES ###
 
-library(data.table)
-library(tidyverse)
-library(ctmm)
-library(sf)
-#for parallel processing
-library(parallel)
-library(foreach)
-library(doParallel)
+### LIBRARIES ###
+# Load necessary libraries
+library(data.table)   # For fast data manipulation
+library(tidyverse)    # For data wrangling
+library(ctmm)         # For continuous-time movement modeling
+library(sf)           # For handling spatial data
+library(parallel)     # For parallel processing
+library(foreach)      # For parallel for loops
+library(doParallel)   # For registering parallel backend
 
-#set time zones
+# Set the time zone to ensure consistent time handling
 Sys.setenv(TZ = 'Europe/Stockholm')
 
-#create data path for transmitter data
-data_filter_path = "./data/tracks_filtered/"
-save_ctmm_path = "./data/ctmm_fits/"
-save_telem_path = "./data/telem_obj/"
+# Define file paths for reading and saving filtered telemetry and ctmm model results
+filtered_data_path <- "./data/tracks_filtered/lake_cow_paradise/"
+save_ctmm_path <- "./data/ctmm_fits/"
+save_telem_path <- "./data/telem_obj/"
 
 #-------------------------------------------------------------------------------#
 
 #> 2. Create telemetry objects
 
 #Load in the datasets
-lake_cow_sub <- readRDS(paste0(data_filter_path, 'lake_cow_sub.rds'))
+lake_cow_sub <- readRDS(paste0(filtered_data_path, '01_lake_cow_sub.rds'))
 
 #Eric - movebank method. Provide as.telemetry with columns names it works better with
 #No need to coerce it into a move object first.
 lake_cow_movebank <- 
   with(lake_cow_sub, 
-       data.frame("timestamp" = timestamp, 
-                  "location.long" = Long,   
-                  "location.lat" = Lat, 
-                  "GPS.HDOP" = HPE,      
-                  "individual-local-identifier" = Fish_ID,
-                  "species" = Species,
-                  "treatment" = Treatment, 
-                  "date" = date,        
-                  "week" = week))
-
-# pike_lake_cow_tel_utm <- as.telemetry(pike_movebank, 
-#                                        timezone = "Europe/Stockholm", 
-#                                        timeformat="%Y-%m-%d %H:%M:%S", 
-#                                        projection= "+init=epsg:32634",
-#                                        datum="WGS84",
-#                                        keep = c("treatment", "date", 
-#                                                 "week", "individual_day"))
+       data.frame(
+         "timestamp" = timestamp_cest,                        
+         "location.long" = Long,                         
+         "location.lat" = Lat, 
+         "GPS.HDOP" = HPE,                               
+         "individual-local-identifier" = individual_ID, 
+         "Species" = Species,
+         "Weight" = Weight,
+         "Total_length" = Total_length,
+         "Std_length" = Std_length,
+         "Treatment" = Treatment,                        
+         "Date" = date_cest,
+         "Exp_Stage" = Stage,
+         "Time_Of_Day" = time_of_day
+       ))
 
 lake_cow_tel_tpeqd <- as.telemetry(lake_cow_movebank, 
-                                         timezone = "Europe/Stockholm", 
-                                         timeformat="%Y-%m-%d %H:%M:%S", 
-                                         projection= NULL,
-                                         datum="WGS84",
-                                         keep = c("treatment", "species","date", 
-                                                  "week"))
+                                   timezone = "Europe/Stockholm", 
+                                   timeformat="%Y-%m-%d %H:%M:%S", 
+                                   projection= NULL,
+                                   datum="WGS84",
+                                   keep = c("Species","Weight","Total_length", "Std_length", "Treatment", 
+                                            "Date", "Exp_Stage", "Time_Of_Day"))
 
 
 #Check some of the parameters
@@ -94,6 +93,7 @@ uere(lake_cow_tel_tpeqd) <- UERE_tpeqd
 #new column now called VAR.xy
 
 #remove reference list
+names(lake_cow_tel_tpeqd)
 lake_cow_tel <- lake_cow_tel_tpeqd[1:66]
 
 #-----------------------------------------------------------------------------#
@@ -109,9 +109,15 @@ lake_cow_tel <- lake_cow_tel_tpeqd[1:66]
 
 #first seperate telemetry object by species 
 #check whether ids are in species order
-lake_cow_movebank %>% 
-  select(species, individual.local.identifier) %>% 
-  distinct()
+lake_cow_movebank %>%
+  select(Species, individual.local.identifier) %>%
+  distinct() %>%
+  arrange(individual.local.identifier)
+
+#need to order them by id number
+lake_cow_tel <- lake_cow_tel[order(names(lake_cow_tel))]
+names(lake_cow_tel)
+
 #mostly in order except for the first perch
 pike_lake_cow_tel <- lake_cow_tel[61:66]
 perch_lake_cow_tel <- lake_cow_tel[c(1,22:60)]
@@ -120,10 +126,8 @@ roach_lake_cow_tel <- lake_cow_tel[2:21]
 #remove outliers based on speed
 #pike
 out_pike <- outlie(pike_lake_cow_tel, plot = FALSE)
-head(out_pike[[1]])
 sum(sapply(out_pike, function(x) sum(x$speed > 0.823)))
-#Function to get range in speeds (m/s) for each individual. 
-#26360 are > than 0.823 m/s 
+#26812
 
 #Need to filter out unrealistic speeds
 #Making a logical vector
@@ -131,14 +135,13 @@ which_lowSp <- lapply(out_pike, function(x) x$speed <= 0.823)
 #Combining the lists and removing observations for which the logical vector was false
 pike_lake_cow_tel <- Map(function(x,y) x[y,], pike_lake_cow_tel,which_lowSp)
 #save telemetry object
-saveRDS(pike_lake_cow_tel , paste0(save_telem_path, "pike_lake_cow_tel.rds")) 
+saveRDS(pike_lake_cow_tel , paste0(save_telem_path, "cow_paradise/pike_lake_cow_tel.rds")) 
 
 #Perch
 out_perch <- outlie(perch_lake_cow_tel, plot = FALSE)
-head(out_perch[[1]])
 sum(sapply(out_perch, function(x) sum(x$speed > 0.977)))
 #Function to get range in speeds (m/s) for each individual. 
-#59017 are > than 0.823 m/s 
+#58011 are > than 0.823 m/s 
 
 #Need to filter out unrealistic speeds
 #Making a logical vector
@@ -146,14 +149,14 @@ which_lowSp <- lapply(out_perch, function(x) x$speed <= 0.977)
 #Combining the lists and removing observations for which the logical vector was false
 perch_lake_cow_tel <- Map(function(x,y) x[y,], perch_lake_cow_tel,which_lowSp)
 #save telemetry object
-saveRDS(perch_lake_cow_tel , paste0(save_telem_path, "perch_lake_cow_tel.rds")) 
+saveRDS(perch_lake_cow_tel , paste0(save_telem_path, "cow_paradise/perch_lake_cow_tel.rds")) 
 
 #Roach
 out_roach <- outlie(roach_lake_cow_tel, plot = FALSE)
 head(out_roach[[1]])
 sum(sapply(out_roach, function(x) sum(x$speed > 0.841)))
 #Function to get range in speeds (m/s) for each individual. 
-#129856 are > than 0.841 m/s 
+#128508 are > than 0.841 m/s 
 
 #Need to filter out unrealistic speeds
 #Making a logical vector
@@ -161,8 +164,38 @@ which_lowSp <- lapply(out_roach, function(x) x$speed <= 0.841)
 #Combining the lists and removing observations for which the logical vector was false
 roach_lake_cow_tel <- Map(function(x,y) x[y,], roach_lake_cow_tel,which_lowSp)
 #save telemetry object
-saveRDS(roach_lake_cow_tel , paste0(save_telem_path, "roach_lake_cow_tel.rds"))
+saveRDS(roach_lake_cow_tel , paste0(save_telem_path, "cow_paradise/roach_lake_cow_tel.rds"))
 
+
+
+#> 5. Make combined dataframe with outliers for each species removed ####
+
+
+# Create a function to extract the data and add the individual_id column
+# This function merges telemetry data from different individuals into a single dataframe.
+extract_data <- function(list_data) {
+  data_combined <- do.call(rbind, lapply(names(list_data), function(id) {
+    df <- list_data[[id]]
+    df$individual_ID <- id  # Add individual ID to each dataframe.
+    return(df)
+  }))
+  return(data_combined)
+}
+
+# Apply the function to each species' telemetry data and combine them into a single dataframe.
+pike_data <- extract_data(pike_lake_cow_tel)
+perch_data <- extract_data(perch_lake_cow_tel)
+roach_data <- extract_data(roach_lake_cow_tel)
+
+# Combine all dataframes into one, with a species label added.
+cow_telem_data <- 
+  rbind(
+    cbind(pike_data, Species = 'Pike'), 
+    cbind(perch_data, Species = 'Perch'),
+    cbind(roach_data, Species = 'Roach')
+  )
+
+saveRDS(cow_telem_data, paste0(filtered_data_path, "02_lake_cow_sub.rds"))
 
 #------------------------------------------------------------------------------#
 
